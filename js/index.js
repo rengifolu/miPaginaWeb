49ba6dacb48e009e57ad7d6f310cd52f97f56b83
rengifolu <andrelujan7@gmail.com>
2018-10-24 00:18:43 +0200

    scroll con javascript para primer apartado y creacion de form en footer

--- js/index.js
+++ js/index.js
@@ -1,9 +1,69 @@
-// para agrandar div de primer apartado
-// function bigImg(x) {
-//     document.getElementById("titulo").style.fontSize = "2em";
-//     document.getElementById("titulo").style.animationDelay = "2s";
+// function apareceScroll(){
+//     var html = document.getElementsByTagName("html")[0];
+//     var elementoAparece = document.getElementsByClassName("logo");
+    
+
+//     document.addEventListener("wheel", function(){
+//         var topVent = html.scrollTop;
+//         for (i = 0; i < elementoAparece.length; i++) {
+//             var topelemAparece = elementoAparece[i].offsetTop;
+//             if (topVent > topelemAparece - 400) {
+//                 elementoAparece[i].style.opacity = 1;
+//             }
+            
+//         }
+//     })
 // }
 
-// function normalImg(x) {
-//     document.getElementById("titulo").style.ontSize = "0.5em";
-// }
+// apareceScroll();
+// console.log("f")
+
+// SE EJECUTA CUANDO INICIA PAGINA
+window.onload = function inic (){
+    var nombre = document.getElementById("nombre");
+    var profesion = document.getElementById("profesion");
+    var foto = document.getElementById("imagen");
+    
+    nombre.style.fontSize = "6em";
+    nombre.style.transitionDuration = "1s";
+
+    profesion.style.fontSize = "4em";
+    profesion.style.transitionDuration = "1s";
+
+    foto.style.height = "50%";
+    foto.style.width = "50%";
+    foto.style.transitionDuration = "1s";
+    
+}
+
+//SCROLL DE BLOQUE DE IMAGEN
+function scrollbBloqueImagen(){
+   var yPos = window.pageYOffset;
+   var nombre = document.getElementById("nombre");
+   var profesion = document.getElementById("profesion");
+   var foto = document.getElementById("imagen");
+
+   if (yPos>200) {
+    nombre.style.fontSize = "5em";
+    nombre.style.transitionDuration = "1s";
+
+    profesion.style.fontSize = "3em";
+    profesion.style.transitionDuration = "1s";
+
+    foto.style.height = "40%";
+    foto.style.width = "40%";
+    foto.style.transitionDuration = "1s";
+   } else {
+    nombre.style.fontSize = "6em";
+    nombre.style.transitionDuration = "1s";
+
+    profesion.style.fontSize = "4em";
+    profesion.style.transitionDuration = "1s";
+
+    foto.style.height = "50%";
+    foto.style.width = "50%";
+    foto.style.transitionDuration = "1s";
+   }
+}
+
+window.addEventListener("scroll", scrollbBloqueImagen);
---
+++ js/scroll.js
@@ -0,0 +1,33 @@
+//SCROLL DE PARALLAX
+function apareceScroll(){
+    var html = document.getElementsByTagName("html")[0];
+    console.log(html)
+    var elementoAparece = document.getElementsByClassName("parallax");
+    
+    // console.log("elementoAparece : "+elementoAparece);
+    
+
+    document.addEventListener("wheel", function(){
+        var topVent = html.scrollTop;
+        // console.log("topVent : "+topVent)
+        for (i = 0; i < elementoAparece.length; i++) {
+            var topelemAparece = elementoAparece[i].offsetTop;
+            // console.log("topelemAparece : "+topelemAparece)
+            if (topVent > (topelemAparece - 400)) {
+                elementoAparece[i].style.opacity = 1;
+                elementoAparece[i].style.borderTop = "6px solid green";
+                elementoAparece[i].style.transitionDuration = "1.5s";
+                
+                // console.log("elementoAparece[i].style.opacity : "+elementoAparece[i].style.opacity)
+            }else{
+                elementoAparece[i].style.opacity = 0;
+                elementoAparece[i].style.transitionDuration = "1.5s";
+            }
+            
+        }
+    })
+}
+
+apareceScroll();
+
+
